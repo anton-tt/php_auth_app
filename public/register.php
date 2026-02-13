3362c781d4ed46398951bc1e35da1286f3a6391b
anton-tt <carduelis@yandex.ru>
2026-02-14 00:39:42 +0700

    feat: Добавлена базовая регистрация пользователя

--- public/register.php
+++ public/register.php
@@ -1,3 +1,34 @@
+<?php
+
+require __DIR__ . '/../config/db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $passwordConfirm = $_POST['password_confirm'];
+
+    if (empty($name) || empty($phone) || empty($email) || empty($password) || empty($passwordConfirm)) {
+        echo "Все поля обязательны для заполнения!";
+        exit;
+    }
+
+    if ($password !== $passwordConfirm) {
+        echo "Пароли не совпадают!";
+        exit;
+    }
+
+    $stmt = $pdo->prepare(
+        "INSERT INTO users (name, phone, email,  password) VALUES (?, ?, ?, ?)"
+    );
+
+    $stmt->execute([$name, $phone, $email, $password]);
+
+    echo "Данные отправлены!";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -14,8 +45,14 @@
         <label>Телефон:</label>
         <input type="text" name="phone" required><br><br>
 
+        <label>Email:</label>
+        <input type="email" name="email" required><br><br>
+
         <label>Пароль:</label>
         <input type="password" name="password" required><br><br>
+
+        <label>Повторите пароль:</label>
+        <input type="password" name="password_confirm" required><br><br>
 
         <button type="submit">Зарегистрироваться</button>
     </form>
