ea1fa02d4879e5786e994352fd27a5947213447a
anton-tt <carduelis@yandex.ru>
2026-02-13 15:58:12 +0700

    Свёрстана форма для регистрации пользователя

---
+++ public/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Регистрация нового пользователя</title>
+</head>
+<body>
+    <h2>Регистрация</h2>
+    <form method="POST">
+        <label>Имя:</label>
+        <input type="text" name="name" required><br><br>
+
+        <label>Телефон:</label>
+        <input type="text" name="phone" required><br><br>
+
+        <label>Пароль:</label>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Зарегистрироваться</button>
+    </form>
+
+</body>
+</html>
