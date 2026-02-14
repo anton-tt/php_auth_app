e902b380df92d19ffce4be8e76f768df8c4407c4
anton-tt <carduelis@yandex.ru>
2026-02-15 00:24:15 +0700

    Свёрстана основа главной страницы

--- public/index.php
+++ public/index.php
@@ -1,4 +1,26 @@
 <?php
 
-require __DIR__ . '/../config/db.php';
-echo "Подключение к базе успешно!";
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная страница</title>
+</head>
+<body>
+    <h1>Главная страница</h1>
+    <p>
+        <a href="login.php">Войти в профиль</a>
+    </p>
+
+    <p>
+        <a href="register.php">Регистрация нового пользователя</a>
+    </p>
+
+    <p>
+        <a href="profile.php">Посмотреть свой профиль</a>
+    </p>
+</body>
+</html>
